49b9d3485b0d025c18e749ede6abd6708e79c2e5
christina <headsteins@gmail.com>
2023-03-19 20:25:00 +0800

    feat: add tauri

--- packages/luna-pc/src-tauri/src/main.rs
+++ packages/luna-pc/src-tauri/src/main.rs
@@ -7,8 +7,19 @@ fn greet(name: &str) -> String {
     format!("Hello, {}! You've been greeted from Rust!", name)
 }
 
+use tauri::{CustomMenuItem, Menu, MenuItem, Submenu};
+
 fn main() {
+    // here `"quit".to_string()` defines the menu item id, and the second parameter is the menu item label.
+    let quit = CustomMenuItem::new("quit".to_string(), "Quit");
+    let close = CustomMenuItem::new("close".to_string(), "Close");
+    let submenu = Submenu::new("File", Menu::new().add_item(quit).add_item(close));
+    let menu = Menu::new() // configure the menu
+        .add_native_item(MenuItem::Copy)
+        .add_item(CustomMenuItem::new("hide", "Hide"))
+        .add_submenu(submenu);
     tauri::Builder::default()
+        .menu(menu)
         .invoke_handler(tauri::generate_handler![greet])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
