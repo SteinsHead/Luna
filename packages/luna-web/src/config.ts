fe3f742376f213c9d307d4289593dfa7aff394d9
christina <headsteins@gmail.com>
2023-04-03 00:10:04 +0800

    feat: 修改首页样式, 添加进入页面按钮

---
+++ packages/luna-web/src/config.ts
@@ -0,0 +1,30 @@
+const config = {
+  github: {
+    username: 'SteinsHead',
+    repository: 'blog',
+    token: ['ghp_L5czx6DXhT72ogtj', '05ckhAjVhU0sn01bCvTA'],
+  },
+  leancloud: {
+    appId: 'LjOwR4ELEqbauKvtQbwq5TUw-MdYXbMMI',
+    appKey: 'BOSFJ49ohbGctgn4zvJE9VN6',
+    serverURL: 'https://api.chanshiyu.com',
+  },
+  contact: {
+    github: 'https://github.com/chanshiyucx',
+    twitter: 'https://twitter.com/chanshiyucx',
+    telegram: 'https://t.me/chanshiyucx',
+    email: 'mailto://me@chanshiyu.com',
+    music: 'https://music.163.com/#/user/home?id=103060582',
+    blog: 'https://chanshiyu.gitbook.io/blog',
+  },
+  gitalk: {
+    clientID: '864b1c2cbc4e4aad9ed8',
+    clientSecret: '288ee5039cf42253b83b5c40cf0cf38770f3213a',
+    repo: 'comment',
+    owner: 'chanshiyucx',
+    admin: ['chanshiyucx'],
+    distractionFreeMode: false,
+  },
+};
+
+export default config;
