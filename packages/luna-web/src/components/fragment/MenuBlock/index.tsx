c54b477008ce832fdc517b0ed276561ec023f2aa
christina <headsteins@gmail.com>
2023-04-25 23:52:11 +0800

    feat: 修改文章样式, 添加切换模式按钮, 效果不错

--- packages/luna-web/src/components/fragment/MenuBlock/index.tsx
+++ packages/luna-web/src/components/fragment/MenuBlock/index.tsx
@@ -1,4 +1,36 @@
+import { Bookshelf, LarkOne, Me, Terminal } from '@icon-park/react';
+
 function MenuBlock({ blockName, blockLink }: MenuProps) {
+  const bookshelf = (
+    <Bookshelf
+      theme="outline"
+      size="20"
+      fill="#fff"
+      style={{ marginTop: '0.5rem' }}
+    />
+  );
+
+  const blog = (
+    <LarkOne
+      theme="outline"
+      size="20"
+      fill="#fff"
+      style={{ marginTop: '0.5rem' }}
+    />
+  );
+
+  const about = (
+    <Me theme="outline" size="20" fill="#fff" style={{ marginTop: '0.5rem' }} />
+  );
+
+  const tech = (
+    <Terminal
+      theme="outline"
+      size="20"
+      fill="#fff"
+      style={{ marginTop: '0.5rem' }}
+    />
+  );
   return (
     <>
       <div
@@ -8,13 +40,35 @@ function MenuBlock({ blockName, blockLink }: MenuProps) {
           flex: 1,
           margin: 5,
           borderRadius: 5,
+          padding: 5,
           display: 'flex',
           justifyContent: 'center',
           alignItems: 'center',
           boxShadow: '0 2px 12px 0 rgb(0 0 0 / 10%)',
+          cursor: 'default',
         }}
       >
-        <p style={{ margin: 0 }}>{blockName}</p>
+        {blockName === 'Diary' ? (
+          bookshelf
+        ) : blockName === 'Blog' ? (
+          blog
+        ) : blockName === 'About' ? (
+          about
+        ) : blockName === 'Tech' ? (
+          tech
+        ) : (
+          <></>
+        )}
+        <p
+          style={{
+            margin: 0,
+            color: '#000',
+            fontWeight: 700,
+            marginLeft: '.5rem',
+          }}
+        >
+          {blockName}
+        </p>
       </div>
     </>
   );
